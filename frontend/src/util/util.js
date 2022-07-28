c9dada9d9dae2440776ddc1291b0f5a3a90920e0
Viltskaa <andrewstyou@gmail.com>
2022-07-29 03:26:15 +0400

    29 07 2022

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -1,5 +1,15 @@
 import { serverRequest } from "../classes/serverReq";
 
-export const applyInvite = (href, id) => {
-    serverRequest.
-}
+export const applyInvite = (href, id) => {};
+
+export const closeModal = () => {
+  document.getElementById("modal").classList.remove("visible");
+};
+
+export const openModal = () => {
+  document.getElementById("modal").classList.add("visible");
+};
+
+export const getStateModal = () => {
+  return document.getElementById("modal").classList.contains("visible");
+};
