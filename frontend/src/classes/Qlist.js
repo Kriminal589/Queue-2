168541d7e42629219a65f8898ec88633ab3391f6
Viltskaa <andrewstyou@gmail.com>
2022-08-11 14:48:47 +0400

    11 08 2022

--- frontend/src/classes/Qlist.js
+++ frontend/src/classes/Qlist.js
@@ -1,113 +1,137 @@
 import { QueueBlock } from "./queue";
 import { serverRequest } from "./serverReq";
 import { $Qmaker } from "./Qmaker";
+import { $notice } from "../classes/notice"
+import { createQueueText } from "../util/util"
+import { getId } from "../util/util"
 
 export class QList {
-  #list = [];
-  constructor() {
-    this.selected = null;
-    this.min = true;
+    #list = [];
+    constructor(canAddQueue = false) {
+        this.selected = null;
+        this.min = true;
+        this.canAddQueue = canAddQueue;
 
-    this.setup();
-  }
-  async parseListOfQueues(id) {
-    const values = await serverRequest.getQueuesById(id);
-    this.#list = [];
-    if (values != -1) {
-      await Promise.all(
-        values.map(async (item) => {
-          const name = (await serverRequest.getQueuePropertyById(item.idQueue))
-            .subjectName;
-          const { idQueue, positionStudent } = item;
-
-          this.#list.push(new QueueBlock({ idQueue, name, positionStudent }));
-        })
-      );
+        this.setup();
     }
-  }
-  clear() {
-    this.#list = [];
-    this.selected = null;
-    this.min = true;
-    this.allMin();
-  }
-  setup() {
-    this.ClickHandler = this.ClickHandler.bind(this);
-    this.backButtonClick = this.backButtonClick.bind(this);
-  }
-  async toHtml() {
-    var html = this.min
-    ? this.#list.map((item) => item.toHtml()).join("")
-    : this.selected.toListHtml(
-        await serverRequest.getListOfStudentInQueueById(this.selected.ID)
-    );
-    html += this.min ? `
-    <div class="QmakerContainer border-2px center-items">
-      <div class="Qmaker" id="Qmaker">
-          <div class="btn about" id="QmakerAdd">Создать очередь</div>
-      </div>
-    </div>
-    ` : '';
-    return html
-  }
-  async render() {
-    //document.getElementById('content-main').insertAdjacentHTML('beforeend', await this.toHtml())
-    document.getElementById('content-main').innerHTML = await this.toHtml()
-    this.addEventListeners()
-  }
-  addEventListeners() {
-    if (this.#list.length > 0) {
-      if (this.min) {
-        this.#list.map((item) => {
-          document
-            .getElementById(item.values.idQueue)
-            .addEventListener("click", this.ClickHandler);
-        });
-        document.getElementById('QmakerAdd').onclick = $Qmaker()
-      } else {
-        document
-          .getElementById("btn__back")
-          .addEventListener("click", this.backButtonClick);
-      }
+
+    async parseListOfQueues() {
+        const values = await serverRequest.getQueuesById(getId());
+        this.#list = [];
+        if (values != -1) {
+            await Promise.all(
+                values.map(async (item) => {
+                    const name = (
+                        await serverRequest.getQueuePropertyById(item.idQueue)
+                    ).subjectName;
+                    const { idQueue, positionStudent } = item;
+
+                    this.#list.push(
+                        new QueueBlock({ idQueue, name, positionStudent }),
+                    );
+                }),
+            );
+        }
     }
-  }
 
-  deleteEventListeners() {
-    if (!this.min) {
-      this.#list.map((item) => {
-        document
-          .getElementById(item.values.idQueue)
-          .removeEventListener("click", this.ClickHandler);
-      });
-      document.getElementById('QmakerAdd').onclick = ''
-    } else {
-      document
-        .getElementById("btn__back")
-        .removeEventListener("click", this.backButtonClick);
+    clear() {
+        this.#list = [];
+        this.selected = null;
+        this.min = true;
+        this.allMin();
     }
-  }
 
-  ClickHandler(event) {
-    console.log("click on q with id >>>", event.target.id);
-    this.selected = this.#list.find((item) => item.ID === +event.target.id);
-    if (this.selected) {
-      this.selected.min = false;
-      this.min = false;
-      this.deleteEventListeners();
-      this.render()
+    setup() {
+
     }
-  }
 
-  backButtonClick() {
-    console.log("click back button");
-    this.allMin();
-    this.min = true;
-    this.selected = null;
-    this.deleteEventListeners();
-    this.render()
-  }
+    async toHtml() {
+        var html = this.min
+            ? this.#list.map((item) => item.toHtml()).join("")
+            : this.selected.toListHtml(
+                  await serverRequest.getListOfStudentInQueueById(
+                      this.selected.ID,
+                  ),
+              );
+        if (this.min && this.canAddQueue) {
+            html += `
+                <div class="QmakerContainer border-2px center-items">
+                <div class="Qmaker" id="Qmaker" data-action="create">
+                    <div class="btn about" id="QmakerAdd">Создать очередь</div>
+                </div>
+                </div>
+            `;
+        }
+        return html;
+    }
 
-  allMin() {
-    this.#list.forEach((item) => (item.min = true));
-  }
+    async render() {
+        document.getElementById("content-main").innerHTML = await this.toHtml();
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        if (this.#list.length > 0) {
+            document.getElementById('content-main').addEventListener('click', e => {
+                const action = e.target.dataset.action
+
+                if (action) {
+                    switch (action) {
+                        case "open" : {
+                            this.selected = this.#list.find((item) => item.ID === +e.target.dataset.id);
+                            this.selected.min = false;
+                            this.min = false;
+                            this.render();
+                            break;
+                        }
+                        case "back" : {
+                            this.allMin();
+                            this.selected = null;
+                            this.render();
+                            break;
+                        }
+                        case "copy" : {
+                            serverRequest.getListOfStudentInQueueById(
+                                +e.target.dataset.id
+                            ).then(item => {
+                                navigator.clipboard.writeText(createQueueText(item))
+                                .then(() => {
+                                    $notice('Очередь скопирована в буфер обмена')
+                                })
+                                .catch(() => {
+                                    $notice('Произошла ошибка при копировании в буфер обмена')
+                                })
+                            })
+                            break;
+                        }
+                        case "create" : {
+                            $Qmaker(async (options) => {
+                                //! await serverRequest.addQueue(options)
+                                await this.render()
+                            })
+                            break;
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    deleteEventListeners() {
+        
+    }
+
+    backButtonClick() {
+        console.log("click back button");
+        this.allMin();
+        this.min = true;
+        this.selected = null;
+        this.deleteEventListeners();
+        this.render();
+    }
+
+    allMin() {
+        this.min = true;
+        this.#list.forEach((item) => (item.min = true));
+    }
 }
